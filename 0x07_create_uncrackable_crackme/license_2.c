8847f06532452deb6ecce1bff191f9f3b283653a
LiveOverflow <liveoverflow@gmail.com>
2015-12-15 14:01:20 +0100

    added license_2.c code

---
+++ 0x07_create_uncrackable_crackme/license_2.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+        if(argc==2) {
+		printf("Checking License: %s\n", argv[1]);
+                int sum = 0;
+                for (int i = 0; i < strlen(argv[1]); i++) {
+			sum+= (int)argv[1][i];	
+		}
+		if(sum==916) {
+			printf("Access Granted!\n");
+		} else {
+			printf("WRONG!\n");
+		}
+	} else {
+		printf("Usage: <key>\n");
+	}
+	return 0;
+}
